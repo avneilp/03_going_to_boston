771b8aa962c1022f0a38f6c2e22c861d87645f51
ty-c1016 <tylerchan586@gmail.com>
2023-11-09 14:51:03 -0500

    wip

---
+++ Game/js/main.js
@@ -0,0 +1,29 @@
+let roll = function () {
+    return Math.ceiling(Math.random() * 6);
+}
+
+let rounds = 0;
+
+document.querySelector(".submit").addEventListener("click", function () {
+   rounds = document.querySelector(".rounds")
+   document.querySelector(".rounds").disabled = true;
+})
+
+let player = 0;
+
+document.querySelector(".first").addEventListener("click", function () {
+    let player1 = roll();
+    let player2 = roll();
+    if (player1 > player2) {
+        player = 1;
+    }
+    else if (player2 > player1) {
+        player = 2;
+    }
+    else {
+        player = Math.ceiling(Math.random() * 2)
+    }
+    document.querySelector(".first_info").textContent = `Player ${player} will go first`;
+})
+
+
